1af5d97c4b880e967be9e359fc12bc389bc01236
Shun Usami <usatie@gmail.com>
2018-09-20 20:15:04 +0900

    :sparkles: Add tutorial comments and small refactorings

--- BitcoinKit-HandsOn/ViewController.swift
+++ BitcoinKit-HandsOn/ViewController.swift
@@ -20,52 +20,80 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         createWalletIfNeeded()
-        //updateLabels()
+        updateLabels()
         
         //testMockScript()
     }
     
     func createWalletIfNeeded() {
         if wallet == nil {
-
+            // TODO: - 1. Walletの作成
+            // TODO: 1-1. Private Keyの生成
+            // let privateKey: PrivateKey = "WRITE ME"
+            
+            // TODO: 1-2. Walletの生成
+            // wallet = "WRITE ME"
+            
+            // TODO: 1-3. Walletの保存
+            // wallet?."WRITE ME"
+            let privateKey = PrivateKey(network: .testnet)
+            wallet = Wallet(privateKey: privateKey)
+            wallet?.save()
         }
     }
     
     func updateLabels() {
+        // TODO: - 2. Addressの表示
+        // TODO: 2-1. Addressのprint
+        // print("WRITE ME")
+        // TODO: 2-2. Addressを表示
+        // addressLabel.text = "WRITE ME"
+        // TODO: 2-3. AddressのQRCodeを表示
+        // qrCodeImageView.image = "WRITE ME"
+        
+        print(wallet?.address.cashaddr)
+        addressLabel.text = wallet?.address.cashaddr
+        qrCodeImageView.image = wallet?.address.qrImage()
 
-//        if let balance = wallet?.balance() {
-//            balanceLabel.text = "Balance : \(balance) satoshi"
-//        }
-    }
-    
-    func updateBalance() {
-
-    }
-    
-    func testMockScript() {
-        do {
-            let result = try MockHelper.verifySingleKey(lockScript: simpleCalculation.lockScript, unlockScriptBuilder: simpleCalculation.unlockScriptBuilder, key: MockKey.keyA)
-            
-            //let result = try MockHelper.verifySingleKey(lockScript: P2PKH.lockScript, unlockScriptBuilder: P2PKH.unlockScriptBuilder, key: MockKey.keyA)
-            
-            //let result = try MockHelper.verifyMultiKey(lockScript: Multisig2of3.lockScript, unlockScriptBuilder: Multisig2of3.unlockScriptBuilder, keys: [MockKey.keyA, MockKey.keyB], verbose: true)
-            print("Mock result: \(result)")
-        } catch let error {
-            print("Mock result: \(error)")
+        // TODO: - 3. Balanceの表示
+        // TODO: 3-1. Balanceの表示
+        // let balance: UInt64 = "WRITE ME"
+        // balanceLabel.text = "Balance : \(balance) satoshi"
+        
+        if let balance = wallet?.balance() {
+            balanceLabel.text = "Balance : \(balance) satoshi"
         }
     }
     
+    func reloadBalance() {
+        // TODO: 3-2. Balanceの更新
+        // "WRITE ME"
+        wallet?.reloadBalance(completion: { [weak self] (_) in
+            DispatchQueue.main.async { self?.updateLabels() }
+        })
+    }
+    
     @IBAction func didTapReloadBalanceButton(_ sender: UIButton) {
-        updateBalance()
+        reloadBalance()
     }
     
     @IBAction func didTapSendButton(_ sender: UIButton) {
         guard let addressString = destinationAddressTextField.text else {
             return
         }
-        
-        do {
 
+        do {
+            // TODO: - 4. 送金する
+            // TODO: 4-1. アドレスの生成
+            // let address: Address = "WRITE ME"
+            
+            // TODO: 4-2. ウォレットから送金 [送金完了後、reloadBalanceもやろう！]
+            // WRITE ME!
+            let address: Address = try AddressFactory.create(addressString)
+            try wallet?.send(to: address, amount: 300, completion: { [weak self] (response) in
+                print("送金完了　txid : ", response ?? "")
+                self?.reloadBalance()
+            })
         } catch {
             print(error)
         }
@@ -73,4 +101,20 @@ class ViewController: UIViewController {
 }
 
 
+
+// MARK: - Hello, Bitcoin Script!以降で使用します
+func testMockScript() {
+    do {
+        let result1 = try MockHelper.verifySingleKey(lockScript: simpleCalculation.lockScript, unlockScriptBuilder: simpleCalculation.unlockScriptBuilder, key: MockKey.keyA)
+        print("Mock result1: \(result1)")
+
+        let result2 = try MockHelper.verifySingleKey(lockScript: P2PKH.lockScript, unlockScriptBuilder: P2PKH.unlockScriptBuilder, key: MockKey.keyA)
+        print("Mock result2: \(result2)")
+
+        let result3 = try MockHelper.verifyMultiKey(lockScript: Multisig2of3.lockScript, unlockScriptBuilder: Multisig2of3.unlockScriptBuilder, keys: [MockKey.keyA, MockKey.keyB], verbose: true)
+        print("Mock result3: \(result3)")
+    } catch let error {
+        print("Mock Script Error: \(error)")
+    }
+}
 
